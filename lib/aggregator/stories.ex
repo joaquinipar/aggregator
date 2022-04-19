41ef03d36c142b5c2d1062ddf4518a14a8de95fd
Joaquin Ipar <jipar@hexacta.com>
2022-04-19 01:01:29 -0300

    Move stories.ex to the correct folder

--- lib/aggregator/stories.ex
+++ lib/aggregator/stories.ex
@@ -13,13 +13,17 @@ defmodule Aggregator.Stories do
   alias Aggregator.Story
 
   defmodule State do
-    defstruct stories: [], stories_content: [], refresh_interval: :timer.seconds(10), ids_ref: nil, content_ref: nil
+    defstruct stories: [],
+              stories_content: [],
+              refresh_interval: :timer.seconds(10),
+              ids_ref: nil,
+              content_ref: nil
   end
 
   # Client Interface
 
   def start_link(_arg) do
-    IO.puts "Starting the Stories process.."
+    IO.puts("Starting the Stories process..")
     GenServer.start_link(__MODULE__, %State{}, name: @name)
   end
 
@@ -27,21 +31,21 @@ defmodule Aggregator.Stories do
   Gets the top 50 stories from the stored state.
   """
   def get_stories do
-    GenServer.call @name, :get_stories
+    GenServer.call(@name, :get_stories)
   end
 
   @doc """
   Gets the story with the associated id.
   """
   def get_story(id) when is_integer(id) do
-    GenServer.call @name, {:get_story, id}
+    GenServer.call(@name, {:get_story, id})
   end
 
   @doc """
   Changes the refresh interval after the next refresh.
   """
   def set_refresh_interval(miliseconds) do
-    GenServer.call @name, {:set_refresh_interval, miliseconds}
+    GenServer.call(@name, {:set_refresh_interval, miliseconds})
   end
 
   # Server Callbacks
@@ -49,11 +53,12 @@ defmodule Aggregator.Stories do
   @doc """
   Saves the initial state and schedules a refresh to update the state after the default interval passed.
   """
-  def init( %State{} = state) do
+  def init(%State{} = state) do
     stories_ids = Fetcher.get_50_best_stories()
     stories_content = get_content_from_ids(stories_ids)
-    if Enum.empty?(stories_content), do:
-        Logger.error("Failed to get stories - The stories weren't refreshed")
+
+    if Enum.empty?(stories_content),
+      do: Logger.error("Failed to get stories - The stories weren't refreshed")
 
     schedule_refresh(state.refresh_interval)
     {:ok, %State{stories: stories_ids, stories_content: stories_content}}
@@ -78,7 +83,7 @@ defmodule Aggregator.Stories do
   {:get_story, id} -> Gets story from state. If there's not present there, it fetches it from the API.
   """
   def handle_call({:set_refresh_interval, miliseconds}, _from, state) do
-    {:reply, :ok, %State{ state | refresh_interval: miliseconds}}
+    {:reply, :ok, %State{state | refresh_interval: miliseconds}}
   end
 
   def handle_call({:get_story, id}, _from, %State{} = state) do
@@ -98,7 +103,7 @@ defmodule Aggregator.Stories do
     Logger.info("Refreshing stories...")
 
     # Fetching the top_stories as non blocking operations
-    task_ids = Task.Supervisor.async_nolink(Task.FetchSupervisor,&Fetcher.get_50_best_stories/0)
+    task_ids = Task.Supervisor.async_nolink(Task.FetchSupervisor, &Fetcher.get_50_best_stories/0)
 
     # Scheduling refresh
     schedule_refresh(state.refresh_interval)
@@ -112,11 +117,14 @@ defmodule Aggregator.Stories do
     Process.demonitor(ref, [:flush])
 
     # Creating an non blocking operation that fetches the stories content asynchcronically
-    multitask = Task.Supervisor.async_nolink(Task.FetchSupervisor,
-    fn ->
-      contents = get_content_from_ids(top_stories)
-      {top_stories, contents}
-   end)
+    multitask =
+      Task.Supervisor.async_nolink(
+        Task.FetchSupervisor,
+        fn ->
+          contents = get_content_from_ids(top_stories)
+          {top_stories, contents}
+        end
+      )
 
     # Saving nolink task refs for matching them when the task sends its result
     {:noreply, %State{state | content_ref: multitask.ref}}
@@ -136,6 +144,9 @@ defmodule Aggregator.Stories do
     # Getting rid of the DOWN message
     Process.demonitor(ref, [:flush])
     Logger.info("Stories were refreshed successfully!")
+
+    Aggregator.WebSocketHandler.send_to_ws_suscribers(stories_content)
+
     # Saving both the stories_id and stories_content at the same moment to avoid having inconsistent state for short periods of time
     {:noreply, %State{state | stories: stories_ids, stories_content: stories_content}}
   end
@@ -145,7 +156,8 @@ defmodule Aggregator.Stories do
     |> Enum.map(&Task.async(fn -> Fetcher.get_story(&1) end))
     |> Enum.map(&Task.await(&1))
     |> Enum.map(fn {:ok, res} -> res.body end)
-    |> Enum.map(fn body_json -> String.replace(body_json, "\n", " ") end) # workaround fail encode when string has '\n'
+    # workaround fail encode when string has '\n'
+    |> Enum.map(fn body_json -> String.replace(body_json, "\n", " ") end)
     |> Enum.map(fn story -> Poison.decode!(story, as: %Story{}) end)
   end
 
@@ -161,11 +173,12 @@ defmodule Aggregator.Stories do
     case Fetcher.get_story(id) do
       {:ok, %HTTPoison.Response{body: story_json}} when story_json != "null" ->
         {:ok, Poison.decode!(story_json, as: %Story{})}
+
       {:ok, %HTTPoison.Response{}} ->
         {:error, "Couldn't find story #{id}"}
+
       {:error, reason} ->
         {:error, reason}
     end
-
   end
 end
